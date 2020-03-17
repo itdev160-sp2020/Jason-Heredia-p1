988172d2d683b6e71d2021950f6fef7dc4d67397
unknown <heredijr@gmatc.matc.edu>
2020-03-17 01:17:21 -0500

    Project 1 completed

---
+++ Project-1/js/scripts.js
@@ -0,0 +1,6 @@
+var welcome = 'Welcome to a small portfolio, ';
+var message = 'demostrating why Tire Rescue is the most beneficial Tire Service!';
+var intro = welcome + message ; 
+var welcomeMsg = document.getElementById('intro');
+
+welcomeMsg.textContent = intro;
